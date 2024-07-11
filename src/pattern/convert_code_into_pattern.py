8a7682417e203ea574f223cc428ff72153d00132
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-11 16:29:46 +0900

    feat: A new feature to compute diff by token

--- src/pattern/convert_code_into_pattern.py
+++ src/pattern/convert_code_into_pattern.py
@@ -1,10 +1,37 @@
 import sys
+import difflib
 sys.path.append("./")
 from source_preprocessor import variable_name_preprocessing, tokenize_python_code
 
 def compute_token_diff(condition, consequent):
+    """トークン化された2つのPythonコード文字列の差分を計算する．
+
+    Args:
+        condition (str): 変更前のPythonコード文字列
+        consequent (str): 変更後のPythonコード文字列
+
+    Returns:
+       list: 差分を示すトークンのリスト．削除されたトークンには「-」、追加されたトークンには「+」が付く．
+    """
     token_condition = tokenize_python_code(variable_name_preprocessing(condition))
     token_consequent = tokenize_python_code(variable_name_preprocessing(consequent))
+    diff = []
 
-if __name__ == "__main__":
-    compute_token_diff()
+    sm = difflib.SequenceMatcher(None, token_condition, token_consequent)
+    for tag, i1, i2, j1, j2 in sm.get_opcodes():
+        if tag == 'replace':
+            for token in token_condition[i1:i2]:
+                diff.append(f"-{token}")
+            for token in token_consequent[j1:j2]:
+                diff.append(f"+{token}")
+        elif tag == 'delete':
+            for token in token_condition[i1:i2]:
+                diff.append(f"-{token}")
+        elif tag == 'insert':
+            for token in token_consequent[j1:j2]:
+                diff.append(f"+{token}")
+        elif tag == 'equal':
+            for token in token_condition[i1:i2]:
+                diff.append(token)
+    
+    return diff
