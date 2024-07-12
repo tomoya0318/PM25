52f9d8d150ee1832d3dc0a4d5b44183ceb55cf8b
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-12 14:15:00 +0900

    test: Slight correction because the input was incorrect

--- test/pattern/test_convert_pattern.py
+++ test/pattern/test_convert_pattern.py
@@ -3,13 +3,13 @@ from pattern.convert_code_into_pattern import compute_token_diff
 
 @pytest.fixture
 def condition():
-    return "i=dic['STRING']"
+    return "i=dic[STRING]"
 
 @pytest.fixture
 def consequent():
-    return "i=dic.get('STRING')"
+    return "i=dic.get(STRING)"
 
 def test_compute_token_diff(condition, consequent):
-    expected_diff = ['i', '=', 'dic', '-[', '+.', '+get', '+(', "'STRING'", '-]', '+)']
+    expected_diff = ['i', '=', 'dic', '-[', '+.', '+get', '+(', "STRING", '-]', '+)']
     diff = compute_token_diff(condition, consequent)
     assert diff == expected_diff
