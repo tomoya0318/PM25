14d2a4e7dd8c80ec3421a6916183ac311510ab67
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-12 15:45:02 +0900

    test: A new test to count the number of pattern occurences

--- test/pattern/test_convert_pattern.py
+++ test/pattern/test_convert_pattern.py
@@ -1,16 +1,43 @@
 import pytest
-from pattern.convert_code_into_pattern import compute_token_diff, merge_consecutive_tokens
+from collections import Counter
+from pattern.convert_code_into_pattern import compute_token_diff, merge_consecutive_tokens, extract_valid_subsequences, update_pattern_counter
 
 @pytest.fixture
-def condition():
-    return "i=dic[STRING]"
+def diff_data():
+    return {
+        "condition": "i = dic",
+        "consequent": "i = dic.get"
+    }
 
 @pytest.fixture
-def consequent():
-    return "i=dic.get(STRING)"
+def tokens1():
+    return ['i=dic', '-[']
 
-def test_sequence_token_diff(condition, consequent):
-    expected_diff = ['i=dic', '-[', '+.get(', 'STRING', '-]', '+)']
-    diff = compute_token_diff(condition, consequent)
+@pytest.fixture
+def tokens2():
+    return ['i=dic', '-[', '+.get']
 
-    assert merge_consecutive_tokens(diff) == expected_diff
+def test_sequence_token_diff(diff_data):
+    expected_diff = ['i=dic', '+.get']
+    diff = compute_token_diff(diff_data["condition"], diff_data["consequent"])
+    output_diff = merge_consecutive_tokens(diff)
+    assert output_diff == expected_diff
+
+def test_extract_valid_subsequences(tokens1):
+    expected_patterns = Counter({
+        ('i=dic','-['): 1,
+    })
+    patterns = extract_valid_subsequences(tokens1)
+    assert patterns == expected_patterns
+
+def test_update_pattern_counter(tokens1, tokens2):
+    expected_patterns = Counter({
+        ('i=dic','-['): 2,
+        ('i=dic','-[', '+.get'): 1,
+        ('i=dic', '+.get'): 1,
+    })
+    
+    pattern_counter = extract_valid_subsequences(tokens1)
+    update_pattern_counter(pattern_counter, tokens2)
+    
+    assert pattern_counter == expected_patterns
