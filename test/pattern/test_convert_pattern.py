db6905e891f531f7ebaa8d533921f6024066ba9a
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-12 14:19:40 +0900

    test: Few change due to output change

--- test/pattern/test_convert_pattern.py
+++ test/pattern/test_convert_pattern.py
@@ -1,5 +1,5 @@
 import pytest
-from pattern.convert_code_into_pattern import compute_token_diff
+from pattern.convert_code_into_pattern import compute_token_diff, merge_consecutive_tokens
 
 @pytest.fixture
 def condition():
@@ -9,7 +9,8 @@ def condition():
 def consequent():
     return "i=dic.get(STRING)"
 
-def test_compute_token_diff(condition, consequent):
-    expected_diff = ['i', '=', 'dic', '-[', '+.', '+get', '+(', "STRING", '-]', '+)']
+def test_sequence_token_diff(condition, consequent):
+    expected_diff = ['i=dic', '-[', '+.get(', 'STRING', '-]', '+)']
     diff = compute_token_diff(condition, consequent)
-    assert diff == expected_diff
+
+    assert merge_consecutive_tokens(diff) == expected_diff
