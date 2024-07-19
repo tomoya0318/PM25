488877027792272d87173bb0c7a5e56ebf47eff2
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-19 22:22:17 +0900

    feat: Add new feature to filter pattern

--- src/pattern/convert_code_into_pattern.py
+++ src/pattern/convert_code_into_pattern.py
@@ -2,6 +2,7 @@ import difflib
 import sys
 from collections import Counter
 from itertools import combinations
+import tokenize
 
 sys.path.append("./src/pattern")
 from source_preprocessor import variable_name_preprocessing, tokenize_python_code
@@ -17,8 +18,12 @@ def compute_token_diff(condition, consequent):
     Returns:
        list: 差分を示すトークンのリスト．削除されたトークンには「-」、追加されたトークンには「+」が付く．
     """
-    token_condition = tokenize_python_code(variable_name_preprocessing(condition))
-    token_consequent = tokenize_python_code(variable_name_preprocessing(consequent))
+    try:
+        token_condition = tokenize_python_code(variable_name_preprocessing(condition))
+        token_consequent = tokenize_python_code(variable_name_preprocessing(consequent))
+    except tokenize.TokenError as e:
+        return []
+
     diff = []
 
     sm = difflib.SequenceMatcher(None, token_condition, token_consequent)
@@ -57,7 +62,7 @@ def merge_consecutive_tokens(diff):
     current_token = diff[0]
 
     for token in diff[1:]:
-        if current_token.startswith(('-', '+', '=')) and token.startswith(current_token[0]):
+        if current_token.startswith(("-", "+", "=")) and token.startswith(current_token[0]):
             current_token += token[1:]
             continue
         merged_diff.append(current_token[1:] if current_token.startswith("=") else current_token)
@@ -82,13 +87,26 @@ def extract_valid_subsequences(tokens):
     for length in range(2, n + 1):  # 部分列の長さを2からnまで変更
         for comb in combinations(range(1, n), length - 1):  # 各長さで部分列を生成（start_tokenを固定）
             subseq = (start_token,) + tuple(tokens[i] for i in comb)
-            if any(token.startswith(('-', '+')) for token in subseq):
+            if any(token.startswith(("-", "+")) for token in subseq):
                 subsequences.append(subseq)
-    
+
     # 部分列の頻度をカウント
     subseq_counter = Counter(subsequences)
-    
+
     return subseq_counter
+
+
+def extract_trigger_sequence(pattern):
+    """パターンからトリガーシーケンスを抽出する．
+
+    Args:
+        pattern (tuple): パターンのトークンシーケンス
+
+    Returns:
+        tuple: トリガーシーケンスのトークンシーケンス
+    """
+    trigger_sequence = tuple(token for token in pattern if token.startswith("=") or token.startswith("-"))
+    return trigger_sequence
 
 
 def update_pattern_counter(counter, new_tokens):
@@ -102,18 +120,37 @@ def update_pattern_counter(counter, new_tokens):
     counter.update(new_subsequences)
 
 
-# debag
-if __name__ == "__main__":
-    # サンプルのトークンリスト
-    tokens1 = ['i=dic', '-[', '+.get(', 'STRING', '-]', '+)']
-    tokens2 = ['i=dic', '-[', '+.get(', 'STRING', '-]', '+)', '+diff']
+def filter_patterns(counter, triggerable_initial, actually_changed, threshold=0.1):
+    """パターンカウンターから指定された条件を満たさないパターンをフィルタリングする
 
-    # 最初のトークンリストから有効な部分列を抽出し、その頻度をカウント
-    pattern_counter = extract_valid_subsequences(tokens1)
+    Args:
+        counter (Counter): パターンカウンター
+        triggerable_initial (Counter): トリガーシーケンスが含まれる初期パッチのカウンター
+        actually_changed (Counter): 実際に変更された統合パッチのカウンター
+        threshold (float): 信頼度の閾値
 
-    # 新しいトークンリストからパターンの頻度を更新
-    update_pattern_counter(pattern_counter, tokens2)
+    Returns:
+        Counter: フィルタリングされたパターンカウンター
+    """
+    filtered_counter = Counter()
+    seen_patterns = set()
 
-    # 抽出された有効な部分列とその頻度を表示
-    for subseq, count in pattern_counter.items():
-        print(f"Pattern: {subseq}, Count: {count}")
+    for pattern, count in sorted(counter.items(), key=lambda x: -len(x[0])):
+        if count <= 1:
+            continue
+        # トリガーシーケンスを抽出
+        trigger_sequence = extract_trigger_sequence(pattern)
+        # 信頼度を計算
+        confidence = (
+            actually_changed[pattern] / triggerable_initial[trigger_sequence]
+            if triggerable_initial[trigger_sequence] > 0
+            else 0
+        )
+        # パターンに削除または追加が含まれているか確認
+        if any(token.startswith(("-", "+")) for token in pattern) and confidence >= threshold:
+            # 同じトークンが含まれる大きいパターンを優先
+            if not any(set(pattern).issubset(set(seen)) for seen in seen_patterns):
+                filtered_counter[pattern] = count
+                seen_patterns.add(pattern)
+
+    return filtered_counter
