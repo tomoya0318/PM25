6d329eaa46f6f795e8cab0765f8756849fac5078
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-21 19:47:00 +0900

    style: fix style by black

--- test/pattern/test_preprocessing.py
+++ test/pattern/test_preprocessing.py
@@ -3,13 +3,16 @@ import os
 import json
 
 from pattern.source_preprocessor import variable_name_preprocessing, tokenize_python_code, extract_diff
+
+
 @pytest.fixture
 def some_data():
-    #テスト用のコード
+    # テスト用のコード
     code = """
     class ${0:NAME}:
     """
     yield code
+
 
 @pytest.fixture
 def json_test_data():
@@ -22,7 +25,7 @@ def json_test_data():
             "author": "ngoldbaum",
             "created_at": "2024-04-08 18:39:40",
             "condition": ["${0:STRING}, ${1:STRING}, '_SUPPORTS_SVE'"],
-            "consequent": ["${0:STRING}, ${1:STRING}, '_SUPPORTS_SVE', 'NOGIL_BUILD'"]
+            "consequent": ["${0:STRING}, ${1:STRING}, '_SUPPORTS_SVE', 'NOGIL_BUILD'"],
         },
         {
             "repository": "numpy/numpy",
@@ -30,28 +33,30 @@ def json_test_data():
             "sha": "12fd2d1b073c1a93e7d6fe23ce27651652779d1a",
             "author": "ngoldbaum",
             "created_at": "2024-04-08 18:39:40",
-            "condition": ["sysconfig.get_config_var(\"Py_GIL_DISABLED\"),"],
-            "consequent": ["NOGIL_BUILD,"]
-        }
+            "condition": ['sysconfig.get_config_var("Py_GIL_DISABLED"),'],
+            "consequent": ["NOGIL_BUILD,"],
+        },
     ]
     # 一時ファイルに書き込む
-    with open('test_data.json', 'w') as file:
+    with open("test_data.json", "w") as file:
         json.dump(test_data, file)
-    yield 'test_data.json'
+    yield "test_data.json"
     # テスト終了後にファイルを削除
-    os.remove('test_data.json')
+    os.remove("test_data.json")
 
-#前処理のテスト用
+
+# 前処理のテスト用
 def test_variable_name_preprocessing(some_data):
     processed_code = variable_name_preprocessing(some_data)
     tokenized_code = tokenize_python_code(processed_code)
-    ans = ['class', 'NAME', ':']
+    ans = ["class", "NAME", ":"]
     assert ans == tokenized_code
 
-#jsonファイルからconditionとconsequentを読み込めるかのテスト
+
+# jsonファイルからconditionとconsequentを読み込めるかのテスト
 def test_extract_diff(json_test_data):
     expected_output = [
         ["${0:STRING}, ${1:STRING}, '_SUPPORTS_SVE'", "${0:STRING}, ${1:STRING}, '_SUPPORTS_SVE', 'NOGIL_BUILD'"],
-        ["sysconfig.get_config_var(\"Py_GIL_DISABLED\"),", "NOGIL_BUILD,"]
+        ['sysconfig.get_config_var("Py_GIL_DISABLED"),', "NOGIL_BUILD,"],
     ]
     assert extract_diff(json_test_data) == expected_output
