a067928c16508dcaec2a9758271d9003b322b159
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-29 01:34:21 +0900

    feat: Add file paths configuration

---
+++ src/constants/path.py
@@ -0,0 +1,9 @@
+"""使用するPATHの一覧
+"""
+import os
+
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+DATA = f"{ROOT}/data"
+RESOURCE = f"{DATA}/resources"
+OUT = f"{DATA}/out"
