8656c89b7eb60fcb9febb1043c7e4a6e1f0f667a
tomoya-n <s266227@wakayama-u.ac.jp>
2024-07-29 03:20:31 +0900

    feat: A new feature to convert a file saved in json to a set

--- src/developer/preprocessor.py
+++ src/developer/preprocessor.py
@@ -1,7 +1,6 @@
 import json
 from tqdm import tqdm
-from constants import path
-from utils.file_processor import get_filename, ensure_dir_exists, decode_unicode_escapes
+from utils.file_processor import ensure_dir_exists, decode_unicode_escapes
 
 
 def extract_author(file_path):
@@ -38,3 +37,18 @@ def save_author_to_json(authors, save_path):
     authors_list = list(authors)  # setをリストに変換
     with open(save_path, "w") as f:
         json.dump(authors_list, f, indent=4, ensure_ascii=False)
+
+
+def json_to_set(save_path):
+    """jsonで保存されているデータを集合に変換
+
+    Args:
+        save_path (str): 保存されているデータへのパス
+
+    Returns:
+    set: JSONファイル内のデータをセットに変換したもの
+    """
+    with open(save_path, "r") as f:
+        authors = set(json.load(f))
+
+    return authors
