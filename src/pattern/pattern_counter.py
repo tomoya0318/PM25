336d42be863c84b543995de3037d029d9acb82af
tomoya-n <s266227@wakayama-u.ac.jp>
2024-08-04 14:17:00 +0900

    feat: Count the Number of pattern types

---
+++ src/pattern/pattern_counter.py
@@ -0,0 +1,26 @@
+import polars as pl
+from utils.file_processor import count_elements_in_json, list_files_in_directory, extract_project_name
+from constants import path
+
+
+if __name__ == "__main__":
+    owner = "numpy"
+    dir_path = f"{path.INTERMEDIATE}/pattern/{owner}"
+    projects = list_files_in_directory(dir_path)
+
+    project_names = [extract_project_name(proj) for proj in projects]
+    element_counts = []
+
+    for project in projects:
+        file_path = f"{dir_path}/{project}"
+        num_elements = count_elements_in_json(file_path)
+        element_counts.append(num_elements)
+
+    # polars DataFrameに変換
+    df = pl.DataFrame({"project": project_names, "elements": element_counts})
+
+    # CSVファイルに出力
+    csv_path = f"{path.RESULTS}/{owner}/pattern_element_counts.csv"
+    df.write_csv(csv_path)
+
+    print(f"Element counts saved to {csv_path}")
