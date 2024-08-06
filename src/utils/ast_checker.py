765d745788c2e9691bb0b21a1a9908806707aab1
tomoya-n <s266227@wakayama-u.ac.jp>
2024-08-06 14:59:13 +0900

    feat: Process strings considering escape characters

--- src/utils/ast_checker.py
+++ src/utils/ast_checker.py
@@ -1,6 +1,5 @@
 import ast
 
-
 def are_ast_equal(code1, code2):
     """
     2つのコード文字列が構文的に等しいかどうかを判定
@@ -13,6 +12,12 @@ def are_ast_equal(code1, code2):
         bool: 2つのコード文字列が構文的に等しい場合はTrue、そうでない場合はFalseを返す
     """
     try:
+        # エスケープ文字を考慮して文字列を処理
+        if isinstance(code1, str):
+            code1 = code1.encode('unicode_escape').decode('utf-8')
+        if isinstance(code2, str):
+            code2 = code2.encode('unicode_escape').decode('utf-8')
+
         # コード文字列をASTにパース
         tree1 = ast.parse(code1)
         tree2 = ast.parse(code2)
