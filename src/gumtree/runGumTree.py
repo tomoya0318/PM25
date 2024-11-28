562f5c1528ee0a72e735964365edb2fb05e6984d
tomoya-n <s266227@wakayama-u.ac.jp>
2024-11-28 19:31:02 +0900

    refactor: few change to imporve perf

--- src/gumtree/runGumTree.py
+++ src/gumtree/runGumTree.py
@@ -30,14 +30,14 @@ class GumTreeResponse:
     actions: list[Action] = field(default_factory=list)
 
 
-def run_GumTree(src_code: str, dest_code: str):
+def run_GumTree(src_code: str, dest_code: str) -> str:
     id: str = str(uuid.uuid4())
     TMP_DIR = os.path.join(path.TMP, id)
     try:
         os.makedirs(TMP_DIR, exist_ok=True)
-        (f"Directory '{TMP_DIR}' created or already exists.")
+        print(f"Directory '{TMP_DIR}' created or already exists.")
     except OSError as e:
-        print(f"Filed to create directory")
+        raise OSError(f"Failed to create directory: {str(e)}")
 
     # 一旦pythonのみ対応，のちに多言語？
     src_path = os.path.join(TMP_DIR, "src.py")
@@ -47,7 +47,7 @@ def run_GumTree(src_code: str, dest_code: str):
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(dest_code)
 
-    # dockerコンテナ立ち上げ用
+    # dockerコンテナ内でgumtreeを処理させる用
     command = [
         "docker",
         "run",
@@ -66,10 +66,11 @@ def run_GumTree(src_code: str, dest_code: str):
     try:
         output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
         response = json.loads(output.stdout)
-        remove_dir(TMP_DIR)
         return json.dumps(response, indent=2)
     except subprocess.CalledProcessError as e:
-        print(f"Error {e.stderr}")
+        raise subprocess.SubprocessError(f"Command execution failed: {e.stderr}")
+    finally:
+        remove_dir(TMP_DIR)
 
 
 if __name__ == "__main__":
@@ -78,6 +79,6 @@ if __name__ == "__main__":
 
     output = run_GumTree(condition, consequent)
     output_path = os.path.join(path.RESULTS, "sample", "gumtree_result.json")
-    if isinstance(output, str):
-        with open(output_path, "w", encoding="utf-8") as f:
-            f.write(output)
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(output)
