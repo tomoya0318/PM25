feda1eca2a30a5e88cdfb5236b81823992c8036c
tomoya-n <s266227@wakayama-u.ac.jp>
2024-12-18 12:40:12 +0900

    Creating a program to retrieve data from opendev

---
+++ src/models/gerrit.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+from models.diff import DiffHunk
+
+
+@dataclass
+class Revision:
+    hash: str
+    committer: str
+    subject: str
+    commit_message: str
+    changed_files: list[str]
+
+
+@dataclass_json
+@dataclass
+class MetaData:
+    hash: str
+    committer: str
+    subject: str
+    commit_message: str
+
+
+@dataclass_json
+@dataclass
+class ChangeData:
+    change_id: str
+    merged_at: datetime
+    revisions: list[Revision]
+
+
+@dataclass_json
+@dataclass
+class DiffData:
+    change_id: str
+    file_name: str
+    merged_at: datetime
+    diff_hunk: DiffHunk
+    base: MetaData
+    target: MetaData
