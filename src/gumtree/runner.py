6857413d1b19d1e6876cd9ef89cdfb6198ce59bf
tomoya-n <s266227@wakayama-u.ac.jp>
2025-01-07 11:02:15 +0900

    pathlibに変更

--- src/gumtree/runner.py
+++ src/gumtree/runner.py
@@ -10,17 +10,17 @@ from utils.file_processor import remove_dir
 
 def run_GumTree(src_code: list, dest_code: list) -> GumTreeResponse:
     id: str = str(uuid.uuid4())
-    TMP_DIR = os.path.join(path.TMP, id)
+    TMP_DIR = path.TMP/id
 
     try:
         os.makedirs(TMP_DIR, exist_ok=True)
         print(f"Directory '{TMP_DIR}' created")
 
         # 一旦pythonのみ対応，のちに多言語？
-        src_path = os.path.join(TMP_DIR, "src.py")
+        src_path = TMP_DIR/"src.py"
         with open(src_path, "w", encoding="utf-8") as f:
             f.write("\n".join(src_code))
-        dest_path = os.path.join(TMP_DIR, "dest.py")
+        dest_path = TMP_DIR/"dest.py"
         with open(dest_path, "w", encoding="utf-8") as f:
             f.write("\n".join(dest_code))
 
