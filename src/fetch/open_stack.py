56903da2a337a973991ce5e5dfcdbf29cc20dd8c
tomoya-n <s266227@wakayama-u.ac.jp>
2025-04-15 16:27:33 +0900

    jsonからdbにデータを保存するように変更

--- src/fetch/open_stack.py
+++ src/fetch/open_stack.py
@@ -9,19 +9,25 @@ from urllib.parse import quote
 
 import requests
 from dotenv import load_dotenv
+from sqlmodel import Session
 
 from constants import path
 from diff.file_diff import get_diff
 from models.diff import DiffHunk
-from models.gerrit import ChangeData, DiffData, MetaData, Revision
+from models.gerrit import ChangeData, DiffData, MetaData, Revision, FileData, MetaDataWithFile
 from utils.diff_handler import DiffDataHandler
-from utils.file_processor import base64_encode
+from utils.file_processor import base64_encode, dump_to_json, load_from_json
 from utils.lang_identifiyer import identify_lang_from_file
+
+# データベースモジュールのインポート
+from database.database import engine, create_db_and_tables
+from database.crud import create_file_data
 
 
 class OpenStackAnalyzer:
     def __init__(
-        self, user_name: str, token: str, start_date: datetime | None = None, end_date: datetime | None = None
+        self, user_name: str, token: str, start_date: datetime | None = None, end_date: datetime | None = None,
+        use_db: bool = True
     ):
         self.user_name = user_name
         self.token = token
@@ -29,6 +35,11 @@ class OpenStackAnalyzer:
         self.base_url = "https://review.opendev.org/a"
         self.start_date = start_date
         self.end_date = end_date
+        self.use_db = use_db
+
+        # データベースとテーブルの初期化
+        if self.use_db:
+            create_db_and_tables()
 
     def _build_headers(self, user_name: str, token: str):
         credentials = f"{user_name}:{token}"
@@ -121,6 +132,112 @@ class OpenStackAnalyzer:
                     revisions=revisions,
                 )
 
+
+    def get_all_changes(self, owner: str, repo: str) -> None:
+        page = 1
+        while True:
+            changes = list(self._fetch_changes(owner, repo, page))
+            if not changes:
+                break
+            for change in changes:
+                file_changes = defaultdict(list)
+                for revision in change.revisions:
+                    # ファイルごとのコミットハッシュを特定
+                    for file in revision.changed_files:
+                        file_changes[file].append(revision.hash)
+
+                # 各ファイルに2つのコミットがないものを除外
+                file_changes = {file: hashes for file, hashes in file_changes.items() if len(hashes) >= 2}
+
+                # 対象ファイルを保存
+                for file_name, hashes in file_changes.items():
+                    try:
+                        if identify_lang_from_file(file_name) != "Python":
+                            continue
+
+                        # 変更前と変更後のファイルを取得
+                        base_file = self._fetch_changed_file(
+                            owner, repo, change.branch, change.change_id, hashes[0], file_name
+                        )
+                        target_file = self._fetch_changed_file(
+                            owner, repo, change.branch, change.change_id, hashes[-1], file_name
+                        )
+                        if base_file == None or target_file == None:
+                            continue
+
+                        # 変更前と変更後のファイルを保存
+                        output_path = self._create_save_path_per_year(owner) / change.change_id
+                        if not output_path.exists():
+                            output_path.mkdir(parents=True, exist_ok=True)
+
+                        with open(output_path / f"{hashes[0]}.py", "w") as f:
+                            f.write(base_file)
+                        with open(output_path / f"{hashes[-1]}.py", "w") as f:
+                            f.write(target_file)
+
+                        # DBに取得したコミット情報を保存
+                        self._store_file_revision_data(change, file_name, hashes, output_path)
+                    except ValueError:
+                        continue
+            page += 1
+
+    # DBに取得したコミット情報を保存
+    def _store_file_revision_data(self,change: ChangeData, file_name: str, hashes: list[str], saved_path: Path) -> None:
+        # ハッシュをキーにした辞書を作成
+        revision_dict = {revision.hash: revision for revision in change.revisions}
+        # 対応するリビジョン情報を取得
+        base_revision = revision_dict[hashes[0]]
+        target_revision = revision_dict[hashes[-1]]
+
+        # コミット情報の作成
+        new_file_data = FileData(
+            change_id=change.change_id,
+            branch=change.branch,
+            file_name=file_name,
+            merged_at=change.merged_at,
+            base=MetaDataWithFile(
+                hash=hashes[0],
+                file_path=f"{saved_path}/{hashes[0]}.py",
+                committer=base_revision.committer,
+                subject=base_revision.subject,
+                commit_message=base_revision.commit_message,
+            ),
+            target=MetaDataWithFile(
+                hash=hashes[-1],
+                file_path=f"{saved_path}/{hashes[-1]}.py",
+                committer=target_revision.committer,
+                subject=target_revision.subject,
+                commit_message=target_revision.commit_message,
+            ),
+        )
+
+        # データベースへ保存
+        try:
+            with Session(engine) as db:
+                create_file_data(db, new_file_data)
+                print(f"データベースに保存しました: {change.change_id}, {file_name}")
+        except Exception as e:
+            print(f"データベースへの保存中にエラーが発生しました: {e}")
+
+    def _create_save_path_per_year(self, owner: str) -> Path:
+        base_path = path.RESOURCE / owner
+
+        start_year = self.start_date.year if self.start_date else ""
+        end_year = self.end_date.year if self.end_date else ""
+
+        if start_year and end_year:
+            year_part = f"{start_year}to{end_year}"
+        elif start_year:
+            year_part = f"from{start_year}"
+        elif end_year:
+            year_part = f"to{end_year}"
+        else:
+            year_part = ""
+
+        # パスを構築
+        return base_path / year_part
+
+    # 変更前と変更後のdiffを対象期間で全て取得
     def get_all_diff(self, owner: str, repo: str) -> None:
         page = 1
         while True:
@@ -156,7 +273,7 @@ class OpenStackAnalyzer:
 
                         diffs = get_diff(base_file, target_file)
 
-                        self._save_to_json(change, file_name, hashes, diffs)
+                        self._save_to_json_for_diff(change, file_name, hashes, diffs)
                     except ValueError:
                         continue
 
@@ -183,7 +300,7 @@ class OpenStackAnalyzer:
         except Exception as e:
             raise e
 
-    def _save_to_json(self, change: ChangeData, file_name: str, hashes: list[str], diffs: list[DiffHunk]) -> None:
+    def _save_to_json_for_diff(self, change: ChangeData, file_name: str, hashes: list[str], diffs: list[DiffHunk]) -> None:
         diff_data = []
         Dh = DiffDataHandler
         # ハッシュをキーにした辞書を作成
@@ -244,10 +361,12 @@ class OpenStackAnalyzer:
 
 if __name__ == "__main__":
     owner = "openstack"
-    repos = ["neutron", "cinder", "keystone", "swift", "glance"]
+    # repos = ["neutron", "cinder", "keystone", "swift", "glance"]
+    repos = ["nova"]
     load_dotenv()
     username = os.getenv("USER_NAME")
     token = os.getenv("OPENSTACK_TOKEN")
+    use_db = True  # データベースを使用するフラグ
     start_year = 2013
     end_year = 2025
     for year in reversed(range(start_year, end_year)):
@@ -256,7 +375,7 @@ if __name__ == "__main__":
                 # 期間を指定
                 start_date = datetime(year, 1, 1)
                 end_date = datetime(year + 1, 1, 1)
-                OSA = OpenStackAnalyzer(username, token, start_date, end_date)
+                OSA = OpenStackAnalyzer(username, token, start_date, end_date, use_db=use_db)
                 # ジェネレーターをリストに変換
-                OSA.get_all_diff(owner, repo)
+                OSA.get_all_changes(owner, repo)
     print("end")
--- src/models/gerrit.py
+++ src/models/gerrit.py
@@ -6,6 +6,7 @@ from dataclasses_json import DataClassJsonMixin, config, dataclass_json
 from models.diff import DiffHunk
 
 
+@dataclass_json
 @dataclass
 class Revision:
     hash: str
@@ -26,11 +27,28 @@ class MetaData:
 
 @dataclass_json
 @dataclass
+class MetaDataWithFile(MetaData):
+    file_path: str
+
+@dataclass_json
+@dataclass
 class ChangeData:
     change_id: str
     branch: str
     merged_at: datetime
     revisions: list[Revision]
+
+
+
+@dataclass_json
+@dataclass
+class FileData(DataClassJsonMixin):
+    change_id: str
+    branch: str
+    file_name: str
+    merged_at: datetime = field(metadata=config(encoder=datetime.isoformat, decoder=datetime.fromisoformat))
+    base: MetaDataWithFile
+    target: MetaDataWithFile
 
 
 @dataclass_json
